883bd1e3931a039809abd6b57832957a5ca2ce20
onnnli <souslayk@gmail.com>
2021-09-06 12:47:09 +0300

    fixed tests

--- src/tests/FilterButton.test.js
+++ src/tests/FilterButton.test.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
 import FilterButton from '../components/buttons/FilterButton/FilterButton';
 
 let container = null;
 beforeEach(() => {
-  container = document.createElement("div");
+  container = document.createElement('div');
   document.body.appendChild(container);
 });
 
@@ -16,29 +16,34 @@ afterEach(() => {
   container = null;
 });
 
-it("changes value when clicked", () => {
+it('changes value when clicked', () => {
   const onChange = jest.fn();
   act(() => {
     render(<FilterButton toggleHandler={onChange} />, container);
   });
 
-  // получаем элемент button и кликаем на него несколько раз
-  const button = document.querySelector("button");
-  expect(button.innerHTML).toBe("<img src=\"filter.svg\" alt=\"filter\" class=\"filter__image\">");
+  const button = document.querySelector('button');
+  expect(button.innerHTML).toBe(
+    '<img src="filter.svg" alt="filter" class="filter__image">'
+  );
 
   act(() => {
-    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
   });
 
   expect(onChange).toHaveBeenCalledTimes(1);
-  expect(button.innerHTML).toBe( "<img src=\"filter.svg\" alt=\"filter\" class=\"filter__image\">");
+  expect(button.innerHTML).toBe(
+    '<img src="filter.svg" alt="filter" class="filter__image">'
+  );
 
   act(() => {
     for (let i = 0; i < 5; i++) {
-      button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
     }
   });
 
   expect(onChange).toHaveBeenCalledTimes(6);
-  expect(button.innerHTML).toBe("<img src=\"filter.svg\" alt=\"filter\" class=\"filter__image\">");
+  expect(button.innerHTML).toBe(
+    '<img src="filter.svg" alt="filter" class="filter__image">'
+  );
 });
